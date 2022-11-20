1cccf5354971f0b45e897687d5889ae0f8ae5e17
Tam Pham <tampham47@live.com>
2022-11-20 20:52:44 +0700

    chore: add dydx

--- src/strategies.ts
+++ src/strategies.ts
@@ -7,43 +7,50 @@ type Strategy = {
 };
 
 const delta = 1.85;
-const tpDelta = 10;
 const qtyDelta = 1;
+const tpDelta = 10;
 
 export const strategies: Strategy[] = [
-  {
-    symbol: 'BTCUSDT',
-    delta: -1.25 * delta,
-    qty: 0.005 * qtyDelta,
-    tp: 2.5 * tpDelta,
-    ticksize: 2,
-  },
-  {
-    symbol: 'ETHUSDT',
-    delta: -1.55 * delta,
-    qty: 0.07 * qtyDelta,
-    tp: 2.5 * tpDelta,
-    ticksize: 2,
-  },
-  {
-    symbol: 'BNBUSDT',
-    delta: -2.45 * delta,
-    qty: 0.3 * qtyDelta,
-    tp: 2.5 * tpDelta,
-    ticksize: 1,
-  },
+  // {
+  //   symbol: 'BTCUSDT',
+  //   delta: -1.25 * delta,
+  //   qty: 0.005 * qtyDelta,
+  //   tp: 2.5 * tpDelta,
+  //   ticksize: 2,
+  // },
+  // {
+  //   symbol: 'ETHUSDT',
+  //   delta: -1.55 * delta,
+  //   qty: 0.07 * qtyDelta,
+  //   tp: 2.5 * tpDelta,
+  //   ticksize: 2,
+  // },
+  // {
+  //   symbol: 'BNBUSDT',
+  //   delta: -2.45 * delta,
+  //   qty: 0.3 * qtyDelta,
+  //   tp: 2.5 * tpDelta,
+  //   ticksize: 1,
+  // },
   {
     symbol: 'SOLUSDT',
-    delta: -3.55 * delta,
-    qty: 3.5 * qtyDelta,
+    delta: -2.75 * delta,
+    qty: 7.5 * qtyDelta,
     tp: 2.5 * tpDelta,
     ticksize: 2,
   },
   {
     symbol: 'SRMUSDT',
     delta: -5.55 * delta,
-    qty: 300 * qtyDelta,
-    tp: 12.5,
+    qty: 440 * qtyDelta,
+    tp: 7.5,
+    ticksize: 4,
+  },
+  {
+    symbol: 'DYDXUSDT',
+    delta: -2.95 * delta,
+    qty: 50 * qtyDelta,
+    tp: 7.5,
     ticksize: 4,
   },
 ];
